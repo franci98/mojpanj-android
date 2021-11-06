4ec4fd70bf772eb9d576dfb62e9fc3e43dc631ab
Franci <franciklavz@gmail.com>
2021-11-06 13:16:44 +0100

    Vpisani meseci

--- app/src/main/java/si/kflabs/mojpanj/data/domain/BeeTaskMonth.kt
+++ app/src/main/java/si/kflabs/mojpanj/data/domain/BeeTaskMonth.kt
@@ -6,4 +6,15 @@ enum class BeeTaskMonth(
     val title: String,
 ) {
     JANUARY("Januar"),
+    FEBRUARY("Februar"),
+    MARCH("Marec"),
+    APRIL("April"),
+    MAY("Maj"),
+    JUNE("Junij"),
+    JULY("Julij"),
+    AUGUST("Avgust"),
+    SEPTEMBER("September"),
+    OCTOBER("Oktober"),
+    NOVEMBER("November"),
+    DECEMBER("December");
 }
