37beb4758d0f2833e6d153959ed556ce24e2b915
Franci <franciklavz@gmail.com>
2021-11-06 13:35:17 +0100

    Barve

--- app/src/main/java/si/kflabs/mojpanj/ui/theme/Color.kt
+++ app/src/main/java/si/kflabs/mojpanj/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package si.kflabs.mojpanj.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Yellow = Color(0xFFFFF951)
+val LightYellow = Color(0xFFffff85)
+val DarkYellow = Color(0xFFc9c60c)
+val Brown = Color(0xFFc68c00)
+val LightBrown = Color(0xFFfdbc42)
+val DarkBrown = Color(0xFF905f00)
