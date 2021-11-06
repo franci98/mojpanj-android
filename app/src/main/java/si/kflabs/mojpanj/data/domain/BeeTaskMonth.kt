8c5fb830f3c95e1ead8e775164ec244fe1e19c20
Franci <franciklavz@gmail.com>
2021-11-06 22:55:37 +0100

    Mesečna opravila

--- app/src/main/java/si/kflabs/mojpanj/data/domain/BeeTaskMonth.kt
+++ app/src/main/java/si/kflabs/mojpanj/data/domain/BeeTaskMonth.kt
@@ -22,51 +22,87 @@ enum class BeeTaskMonth(
         get() {
             return when(this) {
                 JANUARY -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("Čiščenje snega", ""),
+                    BeeTask("Skrb za mir na čebelnjaku", ""),
+                    BeeTask("Priprava in ureditev čebelarskega pribora", ""),
+                    BeeTask("Nabava novih čebeljih panjev, stanic", ""),
+                    BeeTask("Izobraževanje", ""),
+                    BeeTask("Urejanje čebelarskega inventarja", ""),
+                    BeeTask("Žičenje satnikov", ""),
+                    BeeTask("Pridobivanje propolisa", ""),
+                    BeeTask("Urejanje nenaseljenih panjev", ""),
                     // itd.
                 )
                 FEBRUARY -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("Kontrola čebel pri čistilnem izletu", ""),
+                    BeeTask("Pomoč čebeljim družinam, ki so ostale brez hrane", ""),
+                    BeeTask("Čiščenje podničnih vložkov oz. podnic", ""),
+                    BeeTask("Ureditev napajalnika", ""),
+                    BeeTask("Čiščenje snega in skrb za mir na čebelnjaku", ""),
                     // itd.
                 )
                 MARCH -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("Temeljit pregled čebeljih družin", ""),
+                    BeeTask("Preveriti zalogo hrane", ""),
+                    BeeTask("Sajenje medovitih rastlin", ""),
+                    BeeTask("Priprava satnic", ""),
                     // itd.
                 )
                 APRIL -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("Kontrolni pregled čebeljih družin", ""),
+                    BeeTask("Preveriti zalego", ""),
+                    BeeTask("Preveriti zalogo hrane", ""),
+                    BeeTask("Gradnja čim več satnic", ""),
+                    BeeTask("Zbiranje cvetnega prahu", ""),
+                    BeeTask("Izenačevanje družin", ""),
                     // itd.
                 )
                 MAY -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("Kontrolni pregled čebeljih družin", ""),
+                    BeeTask("Preprečevanje rojenja", ""),
+                    BeeTask("Vstavljanje gradilnih satov", ""),
+                    BeeTask("Omogočanje prostora v medišču", ""),
+                    BeeTask("Prevoz čebel na pašo", ""),
                     // itd.
                 )
                 JUNE -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("Kontrolni pregled čebeljih družin", "."),
+                    BeeTask("Vzreja matic", "."),
+                    BeeTask("Vzreja rezervnih družin", "."),
+                    BeeTask("Izrezovanje trotovine", "."),
+                    BeeTask("Točenje medu", "."),
                     // itd.
                 )
                 JULY -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("Pregled čebel", ""),
+                    BeeTask("Točenje medu", ""),
+                    BeeTask("Zamenjava matic", ""),
+                    BeeTask("Izdelava ometencev", ""),
                     // itd.
                 )
                 AUGUST -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("Hranjenje čebel za zimo", ""),
+                    BeeTask("Zagotovitev pomlajevalne paše", ""),
+                    BeeTask("Preprečevanje ropa", ""),
                     // itd.
                 )
                 SEPTEMBER -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("Preprečevanje ropa", ""),
+                    BeeTask("Odstranitev neprimernega medu", ""),
+                    BeeTask("Dopolnitev zimske zaloge hrane", ""),
+                    BeeTask("Kontrola družin, ki smo ji zamenjali matico", ""),
                     // itd.
                 )
                 OCTOBER -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("", ""),
                     // itd.
                 )
                 NOVEMBER -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("", ""),
                     // itd.
                 )
                 DECEMBER -> listOf(
-                    BeeTask("Pluženje snega", "Okoli čebelnjaka pospravimo sneg in omogočimo dostop do vrat."),
+                    BeeTask("", ""),
                     // itd.
                 )
             }
